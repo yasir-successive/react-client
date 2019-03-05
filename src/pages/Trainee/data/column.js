b529cb00df71074e73d39f55fedb13548d8b15e0
yasirsuccessive <mohd.yasir@successive.tech>
2019-03-05 17:21:42 +0530

    Adding Sort, and onClick event to Table

--- src/pages/Trainee/data/column.js
+++ src/pages/Trainee/data/column.js
@@ -1,12 +1,1 @@
-const columns = [
-  {
-    field: 'name',
-    label: 'Name',
-    align: 'center',
-  },
-  {
-    field: 'email',
-    label: 'Email Address',
-  },
-];
-export default columns;
+
